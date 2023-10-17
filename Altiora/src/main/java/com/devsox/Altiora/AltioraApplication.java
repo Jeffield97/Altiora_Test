099b2e92e27f473a829cc3455524f71e70fadc89
Jeffield <jchariguaman@gmail.com>
2023-10-17 15:57:15 -0500

    Configuracion inicial

---
+++ Altiora/src/main/java/com/devsox/Altiora/AltioraApplication.java
@@ -0,0 +1,13 @@
+package com.devsox.Altiora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltioraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltioraApplication.class, args);
+	}
+
+}
---
+++ Altiora/src/test/java/com/devsox/Altiora/AltioraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsox.Altiora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltioraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
