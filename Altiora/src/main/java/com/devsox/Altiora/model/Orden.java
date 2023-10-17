61c83816ec0e6c430edd218999225ec6fe776815
Jeffield <jchariguaman@gmail.com>
2023-10-17 16:09:36 -0500

    Actualizar clase Orden

--- Altiora/src/main/java/com/devsox/Altiora/model/Orden.java
+++ Altiora/src/main/java/com/devsox/Altiora/model/Orden.java
@@ -1,4 +1,58 @@
 package com.devsox.Altiora.model;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Orden {
+    @Id
+    private String codigo;
+    private Date fecha;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @ManyToMany
+    @JoinColumn(table = "orden_articulo")
+    private List<Articulo> articulos = new ArrayList<>();
+
+
+    //Métodos getters y setters
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Articulo> getArticulos() {
+        return articulos;
+    }
+
+    public void setArticulos(List<Articulo> articulos) {
+        this.articulos = articulos;
+    }
 }
