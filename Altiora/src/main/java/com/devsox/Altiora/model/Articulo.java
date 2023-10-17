58a087e81c2d5cd910eb8e67864216822a32e4a7
Jeffield <jchariguaman@gmail.com>
2023-10-17 16:25:29 -0500

    Actualizar clase Articulo

--- Altiora/src/main/java/com/devsox/Altiora/model/Articulo.java
+++ Altiora/src/main/java/com/devsox/Altiora/model/Articulo.java
@@ -1,4 +1,51 @@
 package com.devsox.Altiora.model;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Articulo {
+    @Id
+    private String codigo;
+    private String nombre;
+    private BigDecimal precioUnitario;
+
+    @ManyToMany(mappedBy = "articulos")
+    private List<Orden> ordenes;
+
+    //Métodos getters y setters
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public List<Orden> getOrdenes() {
+        return ordenes;
+    }
+
+    public void setOrdenes(List<Orden> ordenes) {
+        this.ordenes = ordenes;
+    }
 }
